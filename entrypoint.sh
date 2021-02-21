8d3e5a8d258a54ed6a79021b34f00f50d919d6c9
Martin Bonnin <martin@mbonnin.net>
2021-02-21 01:30:53 +0100

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+
+if [ -z $INPUT_BASE_URL ]
+then
+  INPUT_BASE_URL="https://oss.sonatype.org/service/local/"
+fi
+
+JSON=$(curl -d "{\"data\": {\"description\": \"$INPUT_DESCRIPTION\"}}" -u "$INPUT_USERNAME":"$INPUT_PASSWORD" ${INPUT_BASE_URL}staging/profiles/$INPUT_STAGING_PROFILE_ID/start  -H "Content-Type: application/json")
+
+# TODO proper json parsing 😅
+REPOSITORY_ID=$(echo $JSON | sed 's/.*stagedRepositoryId":"\([^"]*\)".*/\1/g')
+
+echo "::set-output name=repository-id::$REPOSITORY_ID"
